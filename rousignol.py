561b4d066dcb2a23223c6981af7e37a4f6f968bb
Daniel Moreira <daniel.moreira.41160@gmail.com>
2023-12-06 07:48:43 +0100

    Fonction pour decrypté l'image

---
+++ rousignol.py
@@ -0,0 +1,75 @@
+from PIL import Image
+
+def bmp_to_tuples(image_path):
+    # Ouvrir l'image BMP
+    img = Image.open(image_path)
+
+     # Convertir l'image en mode "RGB" si elle n'est pas déjà en mode "RGB"
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+
+    # Obtenir les dimensions de l'image
+    largeur, hauteur = img.size
+
+    # Initialiser une liste pour stocker les tuples (Rouge, Vert, Bleu)
+    liste_tuples = []
+
+    # Parcourir chaque pixel de l'image
+    for y in range(hauteur):
+        for x in range(largeur):
+            # Obtenir la couleur du pixel
+            couleur_pixel = img.getpixel((x, y))
+
+            # Ajouter le tuple à la liste
+            liste_tuples.append(couleur_pixel)
+
+    return liste_tuples
+
+# Chemin de l'image BMP
+chemin_image_bmp = "rossignol1.bmp"
+chemin_image_bmp2 = "rossignol2.bmp"
+
+# Appeler la fonction et obtenir la liste de tuples
+liste_couleurs = bmp_to_tuples(chemin_image_bmp)
+liste_couleurs2 = bmp_to_tuples(chemin_image_bmp2)
+
+import numpy as np
+
+from PIL import Image
+
+def extract_key_from_bmp(image_path):
+    # Ouvrir l'image BMP avec Pillow
+    img = Image.open(image_path)
+
+    # Récupérer les dimensions de l'image
+    width, height = img.size
+
+    # Initialiser la clé binaire
+    key_binary = ""
+
+    # Parcourir l'image pixel par pixel
+    for y in range(height):
+        for x in range(width):
+            # Obtenir la valeur du pixel à la position (x, y)
+            pixel_value = img.getpixel((x, y))
+
+            # Convertir la valeur du pixel en binaire (8 bits)
+            pixel_binary = format(pixel_value, '08b')
+
+            # Ajouter le bit de poids faible de la valeur du pixel à la clé
+            key_binary += pixel_binary[-1]
+
+    # Convertir la clé binaire en une chaîne de caractères
+    key_string = ""
+    for i in range(0, len(key_binary), 8):
+        byte = key_binary[i:i + 8]
+        key_string += chr(int(byte, 2))
+
+    return key_string
+
+# Exemple d'utilisation
+image_modifiee = "rossignol2.bmp"
+key = extract_key_from_bmp(image_modifiee)
+print("Clé extraite de l'image BMP :", key)
+
+
